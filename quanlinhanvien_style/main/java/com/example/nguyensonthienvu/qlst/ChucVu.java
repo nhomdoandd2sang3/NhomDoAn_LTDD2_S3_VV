719c2e6096c2e4cfad5b9407e510bf0db7ca3a8b
BT Tuan 4 <37360403+huynhtuanvy1998@users.noreply.github.com>
2019-02-25 20:59:11 +0700

    doan

---
+++ quanlinhanvien_style/main/java/com/example/nguyensonthienvu/qlst/ChucVu.java
@@ -0,0 +1,22 @@
+package com.example.nguyensonthienvu.qlst;
+/**
+ * Enum này để thiết lập chức vụ cho nhân viên
+ * @author drthanh
+ *
+ */
+public enum ChucVu {
+	//Muốn gán được như thế này: TruongPhong("Trưởng Phòng")
+	//thì phải có constructor ChucVu(String cv)
+	TruongPhong("Trưởng Phòng"),
+	PhoPhong("Phó Phòng"),
+	NhanVien("Nhân Viên");
+	private String cv;
+	ChucVu(String cv)
+	{
+		this.cv=cv;
+	}
+	public String getChucVu()
+	{
+		return this.cv;
+	}
+}
