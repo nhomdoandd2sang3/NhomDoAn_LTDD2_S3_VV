979cfdcb2d35315c3839eeeb21b092ef7c818b55
nguyensonthienvu <thienvucdtt6@gmail.com>
2019-02-27 10:45:58 +0700

    comic

---
+++ main_themestyle_NSTV/java/com/example/nguyensonthienvu/themeswitcher/ThemeApplication.java
@@ -0,0 +1,9 @@
+package com.example.nguyensonthienvu.themeswitcher;
+
+import android.app.Application;
+
+public class ThemeApplication extends Application {
+    // App level variable to retain selected spinner value
+    public static int currentPosition;
+
+}
