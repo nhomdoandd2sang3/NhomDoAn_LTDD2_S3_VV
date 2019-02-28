6c5bcc1a8525e016d50c98053d385bafd5bb2301
nguyensonthienvu <thienvucdtt6@gmail.com>
2019-02-28 11:15:33 +0700

    game bau cua _full

--- main_gamebaucua/java/com/example/nguyensonthienvu/gamebaucua/Custom_GridView_BanCo.java
+++ main_gamebaucua/java/com/example/nguyensonthienvu/gamebaucua/Custom_GridView_BanCo.java
@@ -14,19 +14,19 @@ public class Custom_GridView_BanCo extends ArrayAdapter<Integer> {
     Context context;
     int resource;
     Integer[] objects;
-    Integer[] giatien = {0, 100, 200, 300, 400, 500};
+    Integer[] giaTien = {0, 100, 200, 300, 400, 500};
     ArrayAdapter<Integer> adapter;
     public Custom_GridView_BanCo(@NonNull Context context, int resource, @NonNull Integer[] objects) {
         super(context, resource, objects);
         this.context = context;
         this.resource = resource;
         this.objects = objects;
-        adapter = new ArrayAdapter<Integer>(context,android.R.layout.simple_spinner_item, giatien);
+        adapter = new ArrayAdapter<Integer>(context,android.R.layout.simple_spinner_item, giaTien);
     }
 
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View view = View.inflate(context, resource, null);
         ImageView imgBanCo = (ImageView)view.findViewById(R.id.imgBanCo);
         Spinner spinnerGiaTien = (Spinner) view.findViewById(R.id.spinnerGiaTien);
@@ -34,8 +34,8 @@ public class Custom_GridView_BanCo extends ArrayAdapter<Integer> {
         spinnerGiaTien.setAdapter(adapter);
         spinnerGiaTien.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
+            public void onItemSelected(AdapterView<?> parent, View view, int positionspinner, long id) {
+                MainActivity.gtDatCuoc[position] = giaTien[positionspinner];
             }
 
             @Override
