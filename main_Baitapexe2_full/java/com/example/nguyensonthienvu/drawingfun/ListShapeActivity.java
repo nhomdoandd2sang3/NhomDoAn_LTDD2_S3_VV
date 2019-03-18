126afe9a2cf0942f5b1fab39c7243ecfbd56e00a
nguyensonthienvu <thienvucdtt6@gmail.com>
2019-03-18 22:03:04 +0700

    comit 12312

---
+++ main_Baitapexe2_full/java/com/example/nguyensonthienvu/drawingfun/ListShapeActivity.java
@@ -0,0 +1,55 @@
+package com.example.nguyensonthienvu.drawingfun;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class ListShapeActivity extends AppCompatActivity {
+    ListView listView;
+    ArrayList<String> list;
+    ImageButton imageButtonBack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_shape);
+        final Intent intent = getIntent();
+        listView = (ListView)findViewById(R.id.lvShape);
+        list = new ArrayList<>();
+        list.add("Tạo mới");
+        list.add("Hình tròn");
+        list.add("Hình vuông");
+        list.add("Hình tam giác");
+        list.add("Hình chữ nhật");
+        ArrayAdapter adapter = new ArrayAdapter(ListShapeActivity.this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+        imageButtonBack = (ImageButton)findViewById(R.id.ibtBack);
+        imageButtonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String vt = list.get(position);
+                Toast.makeText(ListShapeActivity.this, "fuck you"+list.get(position), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(ListShapeActivity.this, MainActivity.class);
+                intent.putExtra("vị trí",vt);
+                setResult(RESULT_OK,intent);
+               finish();
+            }
+        });
+
+
+    }
+}
