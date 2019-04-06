378130b61245758e92404fa0dff79ff941ae48ca
chihai96 <38007845+chihai96@users.noreply.github.com>
2019-04-06 21:23:34 +0700

    snake game

---
+++ main_snake/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnplay,btnback;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnplay = (Button) findViewById(R.id.btnPlay);
+        btnback = (Button) findViewById(R.id.btnBack);
+
+        btnplay.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this, SnakeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        btnback.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this, .class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
