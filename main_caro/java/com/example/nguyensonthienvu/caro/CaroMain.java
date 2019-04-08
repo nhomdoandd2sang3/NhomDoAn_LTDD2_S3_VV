3210d43e56d7464f5df9b3347a567390554340fd
nguyensonthienvu <thienvucdtt6@gmail.com>
2019-04-08 08:11:33 +0700

    caro
    vu

---
+++ main_caro/java/com/example/nguyensonthienvu/caro/CaroMain.java
@@ -0,0 +1,28 @@
+package com.example.nguyensonthienvu.caro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Random;
+
+import android.app.Activity;
+import android.graphics.Point;
+import android.os.Bundle;
+
+public class CaroMain extends Activity {
+
+    private  MyView myChess;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myChess = (MyView)findViewById(R.id.myView1);
+
+    }
+
+
+
+}
