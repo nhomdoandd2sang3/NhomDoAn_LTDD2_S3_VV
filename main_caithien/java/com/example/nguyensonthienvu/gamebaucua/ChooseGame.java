87e62dd27eadd6a0b146134eb44f5d818b926023
noah5201 <34237390+noah5201@users.noreply.github.com>
2019-04-15 20:44:30 +0700

    cm

---
+++ main_caithien/java/com/example/nguyensonthienvu/gamebaucua/ChooseGame.java
@@ -0,0 +1,58 @@
+package com.example.nguyensonthienvu.gamebaucua;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ChooseGame extends AppCompatActivity {
+    Button btnRan,btnXepHinh,btnPikachu,btnKukube,btnBauCua;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wait);
+        btnRan = (Button) findViewById(R.id.btnRan);
+        btnXepHinh=(Button) findViewById(R.id.btnXepHinh);
+        btnPikachu=(Button)findViewById(R.id.btnPikachu);
+        btnKukube=(Button)findViewById(R.id.btnKukube);
+        btnBauCua=(Button)findViewById(R.id.btnBauCua);
+        btnKukube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChooseGame.this,ManChoi.class);
+                startActivity(intent);
+            }
+        });
+        btnPikachu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChooseGame.this,Pikachu.class);
+                startActivity(intent);
+            }
+        });
+        btnRan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChooseGame.this, MainSnakeActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnBauCua.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChooseGame.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnXepHinh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChooseGame.this, CaroMain.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
